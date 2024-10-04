b96b46e96ba7ec88ceb4585e22dd9bdf69cf7bd2
unknown <mahtok98212@gmail.com>
2024-10-04 09:58:42 +0530

    update the env variable

--- backend/src/middleware/multers3Midleware.ts
+++ backend/src/middleware/multers3Midleware.ts
@@ -10,7 +10,7 @@ const s3 = new S3Client({
   region: process.env.AWS_REGION as string,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY as string,
-    secretAccessKey: process.env.AWS_SECRET_KEY as string,
+    secretAccessKey: process.env.AWS_MY_SECRET_KEY as string,
   },
 });
 
