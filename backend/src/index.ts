19822e481cf3d8f0d4c666c2bd37e3962b1626b1
unknown <mahtok98212@gmail.com>
2024-10-04 10:15:35 +0530

    add the frontend url to the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,10 +10,7 @@ connectDb();
 
 // cors optiosn
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://my-video-hub-backend-api.vercel.app",
-  ],
+  origin: ["http://localhost:5173", "https://my-video-hub.vercel.app"],
   optionSuccessStatus: 200,
 };
 
